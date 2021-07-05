f8634746ad209c2f122ae4e88794aebdc6567ffb
qiuyixianq <qiuyixianq@gmail.com>
2021-07-05 22:16:36 +0800

    Implemented to be config pie / line chart

--- src/components/Dashboard/Dashboard.jsx
+++ src/components/Dashboard/Dashboard.jsx
@@ -1,12 +1,12 @@
 import React, { useState } from 'react';
-import { Doughnut } from 'react-chartjs-2';
+import { Line, Doughnut } from 'react-chartjs-2';
 import { Range } from './Range';
+
 
 const rangeList = ['This Month', 'Last Month', 'Last Three Month'];
 
-const state = {
-    labels: ['January', 'February', 'March',
-        'April', 'May'],
+const pieState = {
+    labels: ['January', 'February', 'March','April', 'May'],
     datasets: [
         {
             label: 'Rainfall',
@@ -29,6 +29,21 @@ const state = {
     ]
 }
 
+const lineState = {
+    labels: ['January', 'February', 'March','April', 'May'],
+    datasets: [
+        {
+            label: 'Rainfall',
+            fill: true,
+            lineTension: 0.5,
+            backgroundColor: 'rgba(75,192,192,1)',
+            borderColor: 'rgba(0,0,0,1)',
+            borderWidth: 2,
+            data: [65, 59, 80, 81, 56]
+        }
+    ]
+}
+
 
 export const Dashboard = () => {
     const [selected, setSelected] = useState(rangeList[0]);
@@ -42,30 +57,50 @@ export const Dashboard = () => {
             </header>
 
             <main>
-                <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
+                <div className=" max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
                     {/* Replace with your content */}
                     <div className="px-4 py-6 sm:px-0">
-                        <div className="border-2 border-gray-300 rounded-lg h-96 p-3" >
+                        <div className="border-2 border-gray-300 rounded-lg p-3" >
                             <Range onRangeChange={setSelected} range={selected} />
-                            {/* <div className="h-72 w-72 flex">
-                                <Doughnut
-                                    data={state}
-                                    options={{
-                                        title: {
-                                            display: true,
-                                            text: 'Average Rainfall per month',
-                                            fontSize: 20
-                                        },
-                                        legend: {
-                                            display: true,
-                                            position: 'right'
-                                        },
-                                        
-                                    }}
-                                />
-                            </div> */}
 
 
+                            <div className="flex mt-10 items-center">
+                                <div className="h-72 w-72 ">
+                                    <Doughnut
+                                        data={pieState}
+                                        options={{
+                                            title: {
+                                                display: true,
+                                                text: 'Average Rainfall per month',
+                                                fontSize: 20
+                                            },
+                                            legend: {
+                                                display: true,
+                                                position: 'right'
+                                            },
+
+                                        }}
+                                    />
+                                </div>
+
+
+                                <div>
+                                    <Line
+                                        data={lineState}
+                                        options={{
+                                            title: {
+                                                display: true,
+                                                text: 'Average Rainfall per month',
+                                                fontSize: 20
+                                            },
+                                            legend: {
+                                                display: true,
+                                                position: 'right'
+                                            }
+                                        }}
+                                    />
+                                </div>
+                            </div>
                         </div>
                     </div>
                     {/* /End replace */}
