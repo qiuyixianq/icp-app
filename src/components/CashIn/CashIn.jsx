f80144e99b595205a54c0a4294d82f93b30a8594
qiuyixianq <qiuyixianq@gmail.com>
2021-07-07 13:23:23 +0800

    UI/UX notify on success & empty input handling

--- src/components/CashIn/CashIn.jsx
+++ src/components/CashIn/CashIn.jsx
@@ -11,18 +11,89 @@ export const CashIn = () => {
     const referenceRef = useRef(null);
     const detailRef = useRef(null);
     const amountRef = useRef(null);
+    const alertRef = useRef(null);
+    const successRef = useRef(null);
 
     //handle submit
     const saveRecord = () => {
-        const newRecord = {
-            category: selectedCategory,
-            reference: referenceRef.current.value,
-            detail: detailRef.current.value,
-            amount: +amountRef.current.value,
-            date: new Date()
+        if (referenceRef.current.value && amountRef.current.value) {
+            const newRecord = {
+                category: selectedCategory,
+                reference: referenceRef.current.value,
+                detail: detailRef.current.value,
+                amount: +amountRef.current.value,
+                date: new Date()
+            }
+
+            //clear input field
+            referenceRef.current.value = detailRef.current.value = amountRef.current.value = '';
+            dispatch(addCashIn(newRecord));
+
+            //display success message for 3s
+            successRef.current.style.display = 'flex';
+            setTimeout(() => {
+                successRef.current.style.display = 'none';
+            }, 3000);
+        } 
+        
+        else {
+            //display error message for 3s
+            alertRef.current.style.display = 'flex';
+            setTimeout(() => {
+                alertRef.current.style.display = 'none';
+            }, 3000);
         }
-        dispatch(addCashIn(newRecord));
+
+
+
     }
+
+    //render alert
+    const renderAlert = () => {
+        return (
+            <div className="hidden w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800 fixed right-5 bottom-5 "
+                ref={alertRef}
+            >
+                <div className="flex items-center justify-center w-12 bg-red-500">
+                    <svg className="w-6 h-6 text-white fill-current" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M20 3.36667C10.8167 3.36667 3.3667 10.8167 3.3667 20C3.3667 29.1833 10.8167 36.6333 20 36.6333C29.1834 36.6333 36.6334 29.1833 36.6334 20C36.6334 10.8167 29.1834 3.36667 20 3.36667ZM19.1334 33.3333V22.9H13.3334L21.6667 6.66667V17.1H27.25L19.1334 33.3333Z" />
+                    </svg>
+                </div>
+
+                <div className="px-4 py-2 -mx-3">
+                    <div className="mx-3">
+                        <span className="font-semibold text-red-500 dark:text-red-400">Error</span>
+                        <p className="text-sm text-gray-600 dark:text-gray-200">Required input is empty</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+    //render success
+    const renderSuccess = () => {
+        return (
+
+            <div className="hidden w-full max-w-sm mx-auto overflow-hidden bg-white rounded-lg shadow-md dark:bg-gray-800 fixed right-5 bottom-5 "
+                ref={successRef}
+            >
+                <div className="flex items-center justify-center w-12 bg-green-500">
+                    <svg className="w-6 h-6 text-white fill-current" viewBox="0 0 40 40" xmlns="http://www.w3.org/2000/svg">
+                        <path d="M20 3.33331C10.8 3.33331 3.33337 10.8 3.33337 20C3.33337 29.2 10.8 36.6666 20 36.6666C29.2 36.6666 36.6667 29.2 36.6667 20C36.6667 10.8 29.2 3.33331 20 3.33331ZM16.6667 28.3333L8.33337 20L10.6834 17.65L16.6667 23.6166L29.3167 10.9666L31.6667 13.3333L16.6667 28.3333Z" />
+                    </svg>
+                </div>
+
+                <div className="px-4 py-2 -mx-3">
+                    <div className="mx-3">
+                        <span className="font-semibold text-green-500 dark:text-green-400">Success</span>
+                        <p className="text-sm text-gray-600 dark:text-gray-200">New record has been saved!</p>
+                    </div>
+                </div>
+            </div>
+        )
+    }
+
+
 
     //render amountInput
     const renderCashInput = () => {
@@ -62,7 +133,9 @@ export const CashIn = () => {
 
     //main render
     return (
-        <div>
+        <div className="dark">
+            {renderAlert()}
+            {renderSuccess()}
             <header className="bg-white shadow">
                 <div className="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
                     <h1 className="text-3xl font-bold text-gray-900">Cash In</h1>
@@ -70,7 +143,7 @@ export const CashIn = () => {
             </header>
 
 
-            <main className="dark">
+            <main>
                 <div className="max-w-7xl mx-auto py-6 sm:px-6 lg:px-8">
                     <div className="px-4 py-6 sm:px-0">
                         <div className="border-2 border-gray-300 rounded-lg flex" >
